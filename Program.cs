15e9875e2d82a851b3a7a570f390060b7f8413ef
Mrgaton <arnaupost4@gmail.com>
2023-11-26 00:41:53 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CrashMePlease
+{
+    internal static class Program
+    {
+        private static void Main() => CrashPc();
+
+        [DllImport("ntdll")] private static extern IntPtr RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
+
+        [DllImport("ntdll")] private static extern IntPtr NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
+
+        private static void CrashPc()
+        {
+            RtlAdjustPrivilege(19, true, false, out bool previousValue);
+            NtRaiseHardError(0xC0000420, 0, 0, IntPtr.Zero, 6, out uint response);
+        }
+    }
+}
